2538aecbec29a741202603a83ccfaa7feb7f963e
yangman-git <63397721+yangman-git@users.noreply.github.com>
2020-05-18 18:12:32 +0900

    db chapter

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,39 @@
-export const join = (req, res) => res.render("Join", { pageTitle: "Join" });
-export const login = (req, res) => res.render("Login", { pageTitle: "Login" });
-export const logout = (req, res) =>
-  res.render("Logout", { pageTitle: "Logout" });
-export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To Do: Register User
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const userDetail = (req, res) =>
-  res.render("User Detail", { pageTitle: "User Detail" });
+  res.render("userDetail", { pageTitle: "User Detail" });
+
 export const editProfile = (req, res) =>
-  res.render("Edit Profile", { pageTitle: "Edit Profile" });
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+
 export const changePassword = (req, res) =>
-  res.render("Change Password", { pageTitle: "Change Password" });
+  res.render("changePassword", { pageTitle: "Change Password" });
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,19 @@
 import express from "express";
+import routes from "../routes";
+import {
+  videoDetail,
+  editVideo,
+  deleteVideo,
+  getUpload,
+  postUpload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+videoRouter.get(routes.videoDetail(), videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
